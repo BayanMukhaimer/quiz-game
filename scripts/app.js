57938170940f83a3d6b134da4837dbd5e21a78d4
Bayan <bayanessam40@gmail.com>
2025-07-14 11:35:35 +0300

    initial push

---
+++ scripts/app.js
@@ -0,0 +1,169 @@
+const questions = {
+  Science: [
+    {
+      question: "What gas do plants absorb from the atmosphere?",
+      choices: ["Oxygen", "Nitrogen", "Carbon Dioxide", "Hydrogen"],
+      answer: "Carbon Dioxide"
+    },
+    {
+      question: "What planet is known as the Red Planet?",
+      choices: ["Earth", "Venus", "Mars", "Jupiter"],
+      answer: "Mars"
+    },
+    {
+      question: "Which part of the cell contains genetic material?",
+      choices: ["Newton", "Joule", "Watt", "Pascal"],
+      answer: "Newton"
+    },
+    {
+      question: "Which of the following is not a vector quantity?",
+      choices: ["Velocity", "Force", "Temperature", "Acceleration"],
+      answer: "Temperature"
+    },
+    {
+        sound: "assest\volcano_d4RRnaNI.wav",
+      question: "What natural phenomenon is producing this sound?",
+      choices: ["Earthquake", "Thunderstorm", "Volcano", "Waterfall"],
+      answer: "Volcano"
+    },
+    {
+      question: "What is the pH of a neutral solution?",
+      choices: ["4", "7", "14", "0"],
+      answer: "7"
+    },
+    {
+      question: "What is the SI unit of force?",
+      choices: ["Newton", "Joule", "Watt", "Pascal"],
+      answer: "Newton"
+    },
+    {
+        image: "assest\Human_skeleton_front_en.svg.png",
+      question: "What is the longest bone in the human body?",
+      choices: ["Humerus", "Femur", "Tibia", "Radius"],
+      answer: "Femur"
+    },
+    {
+      question: "What is the chemical symbol for gold?",
+      choices: ["Au", "Ag", "Gd", "Go"],
+      answer: "Au"
+    },
+    {
+        image: "assest\earthLayers.jpg",
+      question: "What is the outermost layer of the Earth called?",
+      choices: ["Mantle", "Core", "Crust", "Lithosphere"],
+      answer: "Crust"
+    }
+  ],
+   History: [
+    {
+      question: "Who was the first President of the USA?",
+      choices: ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "John Adams"],
+      answer: "George Washington"
+    },
+    {
+      question: "In which year did World War II end?",
+      choices: ["1940", "1945", "1939", "1950"],
+      answer: "1945"
+    },
+    {
+      question: "Who was the British Prime Minister during most of World War II?",
+      choices: ["Winston Churchill", "Neville Chamberlain", "Clement Attlee", "Margaret Thatcher"],
+      answer: "Winston Churchill"
+    },
+    {
+      question: "The Great Wall of China was primarily built to:",
+      choices: ["Control flooding", "Mark territory boundaries", "Protect against invasions", "Improve trade routes"],
+      answer: "Protect against invasions"
+    },
+    {
+      question: "Who discovered America in 1492?",
+      choices: ["Vasco da Gama", "Christopher Columbus", "Ferdinand Magellan", "Marco Polo"],
+      answer: "Christopher Columbus"
+    },
+    {
+        image: "assest\hiroshima.jpg",
+      question: "What event is depicted in this image?",
+      choices: ["The Battle of Britain", "The bombing of Pearl Harbor", "The dropping of the atomic bomb on Hiroshima", "The D-Day landings "],
+      answer: "The dropping of the atomic bomb on Hiroshima"
+    },
+    {
+      question: "What event is considered the start of the French Revolution?",
+      choices: ["Execution of King Louis XVI", "Declaration of the Rights of Man", "Storming of the Bastille", "Reign of Terror"],
+      answer: " Storming of the Bastille"
+    },
+    {
+      question: "Who was the famous queen of ancient Egypt known for her beauty and alliance with Rome?",
+      choices: ["Nefertiti", "Hatshepsut", "Isis", "Cleopatra"],
+      answer: "Cleopatra"
+    },
+    {
+        image: "assest\Hadrians-Wal.jpg",
+      question: "Which historical structure is shown in the image?",
+      choices: ["Berlin Wall", "Great Wall of China", "Hadrian's Wall", "Western Wall"],
+      answer: "Hadrian's Wall"
+    },
+    {
+      question: "Which civilization is considered the oldest in the world? ",
+      choices: ["Egyptian Civilization", "Mesopotamian Civilization", "Chinese Civilization", "Indus Valley Civilization"],
+      answer: "Mesopotamian Civilization"
+    }
+    
+  ],
+   Sports: [
+    {
+      question: "In which city were the first modern Olympic Games held?",
+      choices: ["Paris", "London", "Athens", "Rome"],
+      answer: "Athens"
+    },
+    {
+      question: "What is the name of the trophy awarded to the winner of the FIFA World Cup?",
+      choices: ["The Stanley Cup", "The Lombardi Trophy", "The Jules Rimet Trophy", "The Web Ellis Cup"],
+      answer: "The Jules Rimet Trophy"
+    },
+    {
+        image: "assest\Simone_Biles.jpg",
+      question: "Identify the athlete in the image.",
+      choices: ["Michael Phelps", "Usain Bolt", "Simone Biles", "Nadia Comăneci"],
+      answer: "Simone Biles"
+    },
+    {
+        sound: "assest\Official NBA Whistle SFX.mp3",
+      question: "Which sport typically begins with a whistle?",
+      choices: ["Golf", "Basketball", "Chess", "Snooker"],
+      answer: "Basketball"
+    },
+    {
+      question: "Which Olympic athlete has won the most medals?",
+      choices: ["Michael Phelps", "Usain Bolt", "Simone Biles", "Nadia Comăneci"],
+      answer: "Michael Phelps"
+    },
+    {
+        image: "assest\Italy_Circuit.avif",
+      question: 'Which circuit is known as the "Temple of Speed" due to its high average lap speeds?',
+      choices: ["Silverstone", "Monza", "Spa-Francorchamps", "Suzuka"],
+      answer: "Monza"
+    },
+      {
+          question: "Which sport does this sound resemble (grunts and racket hit)?",
+          choices: ["Badminton", "Baseball", "Tennis", "Hockey"],
+          answer: "Tennis"
+        },
+        {
+            question: "Who has won the most Grand Slam titles in men’s tennis (as of 2024)?",
+            choices: ["Berlin Wall", "Rafael Nadal", "Novak Djokovic", "Andy Murray"],
+            answer: "Novak Djokovic"
+        },
+        {
+            question: "What is the national sport of Japan?",
+            choices: ["Baseball", "Karate", "Judo", "Sumo wrestling"],
+            answer: "Sumo wrestling"
+        },
+        {
+        image: "assest\Belgium_Circuit.avif",
+        question: 'Which of these circuits is known for its challenging "Eau Rouge" corner?',
+        choices: ["Silverstone", "Monza", "Spa-Francorchamps", "Suzuka"],
+        answer: "Spa-Francorchamps"
+        }
+    
+  ]
+};
